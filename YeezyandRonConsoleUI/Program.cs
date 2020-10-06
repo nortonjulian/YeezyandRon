257acbe311b64d0d79c050a9544842430ae5a5e9
Julian Norton <nortonjulian@gmail.com>
2020-10-06 17:42:09 -0400

    Ye

---
+++ YeezyandRonConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace YeezyandRonConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KanyeSwanson.YeQuote();
+
+            KanyeSwanson.RonQuote();
+
+        }
+    }
+}
